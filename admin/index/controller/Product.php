7bb64edee15c8cb6553618cbf09d9261ae98f4e6
17693700@qq.com <17693700@qq.com>
2017-05-08 22:26:30 +0800

    无限级分类

--- admin/index/controller/Product.php
+++ admin/index/controller/Product.php
@@ -8,16 +8,13 @@ class Product extends Controller
 {
     public function product_category()
     {
-        $m = Db::name('t_goods_catalog')->where('cat_id',2)->find();
-        var_dump($m);
-
-        echo Db::table('t_goods_catalog')->where('cat_id',1)->value('cat_name');
 
         return $this->fetch();
     }
 
     public function product_add()
     {
+
         return $this->fetch();
     }
 
@@ -28,12 +25,59 @@ class Product extends Controller
 
     public function product_category_add()
     {
+        $m = Db::name('t_goods_catalog');
+        $data=$m->select();
+
+        $new_data = array();
+        
+        foreach($data as $key=>$val){
+            $str = '';
+
+            if($val['level'] > 1){
+                for($i=1;$i<$val['level'];$i++){
+                    $str .= '—';
+                }
+
+                $val['cat_name'] = $str.$val['cat_name'];
+            }
+
+            $new_data[$key] = $val;
+        }
+
+        $this->assign('data',$new_data);
+
         return $this->fetch();
     }
 
     public function product_list()
     {
         return $this->fetch();
+    }
+
+
+    public function category_add(){
+        $data               = array();
+        $data['parent_id']  = $_POST['parent_id'];
+        $data['cat_name']       = $_POST['cat_name'];
+
+        if($data['cat_name'] != ''){
+            $m = Db::name('t_goods_catalog');
+            $parent_data = $m->where('cat_id='.$data['parent_id'])->select();
+
+            $data['level']      = $parent_data[0]['level'] + 1;
+
+            $res = $m->insert($data);
+            if($res){
+                echo "<script>alert('添加成功！');parent.location.href='product_category'</script>";
+            }else{
+                echo "<script>alert('添加失败！');parent.location.href='product_category'</script>";
+            }
+        }else{
+            echo "<script>alert('添加失败，内容不能为空！');parent.location.href='product_category'</script>";
+        }
+        
+        
+
     }
    
 }
